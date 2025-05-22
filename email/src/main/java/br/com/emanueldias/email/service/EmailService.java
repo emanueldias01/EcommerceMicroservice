1305840aceec443df03522eb5021d9ef0e6628e5
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-05-22 10:48:00 -0300

    add anotacao service

--- email/src/main/java/br/com/emanueldias/email/service/EmailService.java
+++ email/src/main/java/br/com/emanueldias/email/service/EmailService.java
@@ -4,7 +4,9 @@ import br.com.emanueldias.email.dto.InfoPedidoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
 
     @Autowired
